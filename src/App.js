182afbf6a4f5ef20a1f9bcb684d2a5463f1fe3bc
Jesmin Sheladiya <119096904+Jesmin-1832@users.noreply.github.com>
2024-03-01 13:57:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Userform from './component/Userform';
+
+function App() {
+  return (
+    <div className="App">
+      <section>
+        <Userform />
+      </section>
+    </div>
+  );
+}
+
+export default App;
